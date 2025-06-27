22c686f2fe3d964b03328a30231c1ef1b229311b
Kritika <kritisarva@gmail.com>
2025-06-27 15:54:26 +0530

    modification in config file

--- src/component/firebase/Config.js
+++ src/component/firebase/Config.js
@@ -2,19 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth,GoogleAuthProvider} from "firebase/auth"
+import dotenv from "dotenv";
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCO5tIejHKs3asqRejyMhgNYkaqarCjd7g",
-  authDomain: "projectcollaborationhub-25874.firebaseapp.com",
-  projectId: "projectcollaborationhub-25874",
-  storageBucket: "projectcollaborationhub-25874.firebasestorage.app",
-  messagingSenderId: "499378089043",
-  appId: "1:499378089043:web:7b556cb947d296457a6aa6",
-  measurementId: "G-4BCJPHR95L"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
